1ee6c864d99a6c5467eb0914d2422bfd6df07974
IT012307 <it012307@MacBooks-MacBook-Pro.local>
2023-03-03 18:43:10 +0530

    make math module

---
+++ modules/index.mjs
@@ -0,0 +1,4 @@
+// const math = require('./math.js')
+import { add, sub} from "./math.mjs"
+console.log(add(1,3));
+console.log(sub(6,1));
---
+++ modules/math.mjs
@@ -0,0 +1,14 @@
+export function add(a,b)  {
+    return a+b;
+}
+export function sub(a,b) {
+    return a-b;
+}
+// module.exports = {
+//     add : add
+// }
+
+// export default {
+//     add: add,
+//     sub: sub
+// }
