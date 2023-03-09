d50fe1d365fad24624f544eac109c5f669e96e9b
IT012307 <it012307@MacBooks-MacBook-Pro.local>
2023-03-09 12:35:11 +0530

    modified updateLocation function

--- weatherApp/modules/modify.mjs
+++ weatherApp/modules/modify.mjs
@@ -1,17 +1,19 @@
-import { weatherData as db} from "./weatherDb.mjs";
+import { weatherData as db } from "./weatherDb.mjs";
 
 // function to add a new location
-function addLocation(location, tempC) {
-  db.push({ location: location, tempC: tempC });
+export function addLocation(location, tempC, condition) {
+  db.push({ location: location, tempC: tempC, condition: condition });
 }
 
 // function to update a location data
-export function updateLocation(location, tempC) {
+export function updateLocation(location, tempC, condition) {
   return db.filter(function (ele) {
     if (ele.location != location) {
       return ele;
     } else {
-      return (ele.tempC = tempC);
+      ele.tempC = tempC;
+      ele.condition = condition;
+      return ele;
     }
   });
 }
@@ -36,10 +38,9 @@ export function getLocationNames() {
 
 // function to delete a location
 export function deleteLocation(location) {
-  for (let i=0; i<db.length; i++){
-    if(db[i].location === location){
-      db.splice(i,1);
+  for (let i = 0; i < db.length; i++) {
+    if (db[i].location === location) {
+      db.splice(i, 1);
     }
   }
 }
-
