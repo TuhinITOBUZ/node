2a6b53de46896262c533ba2c7b63ffb4ab17d019
Tuhin Mukherjee <tuhinmuherjee74@gmail.com>
2023-03-09 21:42:18 +0530

    made text db

--- weatherApp/modules/file.mjs
+++ weatherApp/modules/file.mjs
@@ -1,11 +1,32 @@
 import * as fs from 'fs';
 import { addLocation, updateLocation, deleteLocation } from './weatherOperations.mjs';
 
-fs.readFile('db.txt', function (err, data){
-  if (err) {
-    return console.error(err)
-  }
+
+// fs.readFile('./db.txt', 'utf8', function (err, data){
+//   if (err) {
+//     return console.error(err)
+//   }
+//   const arr = JSON.parse(data)
+//   // console.log(arr)
+
+//   addLocation(arr, 'Kerala', 25, 'Clear')
+
+//   updateLocation(arr, 'Kerala', 30, 'Sunny' )
+
+//   deleteLocation(arr, 'Kerala')
+
+//   fs.writeFile('db.txt', JSON.stringify(arr), function(err){
+//     if (err) {
+//       return console.error(err)
+//     }
+//   })
+// })
+
+
+try {
+  const data = fs.readFileSync('db.txt', 'utf8');
   const arr = JSON.parse(data)
+  // console.log(arr)
 
   addLocation(arr, 'Kerala', 25, 'Clear')
 
@@ -18,4 +39,6 @@ fs.readFile('db.txt', function (err, data){
       return console.error(err)
     }
   })
-})
+} catch (err) {
+  console.error(err);
+}
